405213c2da36e86ef0837f859af17de908507720
Cristhian <motochecristhian@gmail.com>
2022-04-18 21:04:34 -0500

    Nueva Pagina 3

---
+++ MAS3/PageTres.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MAS3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PageTres : ContentPage
+    {
+        public PageTres()
+        {
+            InitializeComponent();
+        }
+    }
+}
