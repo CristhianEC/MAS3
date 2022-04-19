853eb6bfc102d8ad035e5ed9110a31fabd3f9619
master <master@SMLAB408PC11_22>
2022-04-18 20:59:42 -0500

    Agregar archivos de proyecto.

---
+++ MAS3/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MAS3
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void BtnAbrir_Clicked(object sender, EventArgs e)
+        {
+
+            await Navigation.PushAsync(new PageDos(TxtUsuario.Text, TxtPass.Text));
+        }
+    }
+}
---
+++ MAS3/PageDos.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MAS3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PageDos : ContentPage
+    {
+        public PageDos()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Constructor que recibe parámetros
+        /// </summary>
+        /// <param name="strUsuario">Usuario</param>
+        /// <param name="strPassword">Password</param>
+        public PageDos(string strUsuario, string strPassword)
+        {
+            InitializeComponent();
+            LblUsuario.Text = strUsuario;
+            LblPassword.Text = strPassword;
+        }
+
+    }
+}
